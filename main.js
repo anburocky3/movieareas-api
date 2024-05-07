41bc299e2338f2147bb52dc5f7e2b01fa40ca625
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2024-05-07 18:44:19 +0530

    route structure

--- main.js
+++ main.js
@@ -7,6 +7,20 @@ app.get("/", (req, res) => {
   res.json({ msg: "Hello students!" });
 });
 
+// CRUD functionality of movies
+
+// R - For Reading
+app.get("/movies", () => {});
+
+// C - For creating movies
+app.post("/movies", () => {});
+
+// U - For updating movie
+app.put("/movies/:id", () => {});
+
+//  D - For deleting movie
+app.delete("/movies/:id", () => {});
+
 app.listen(PORT, () => {
   console.log(`The server is running at http://localhost:${PORT}`);
 });
